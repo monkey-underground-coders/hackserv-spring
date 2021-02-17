260996e5f80664903ab3c81c67720ceb112170af
Konstantin "6rayWa1cher" Grigorev <info@a6raywa1cher.com>
2021-02-17 17:35:22 +0300

    swagger fixed

--- src/main/java/com/a6raywa1cher/hackservspring/config/WebConfig.java
+++ src/main/java/com/a6raywa1cher/hackservspring/config/WebConfig.java
@@ -4,7 +4,6 @@ import com.a6raywa1cher.hackservspring.utils.resolver.UserHandlerMethodArgumentR
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,7 +11,6 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     private final UserHandlerMethodArgumentResolver userHandlerMethodArgumentResolver;
     AppConfigProperties properties;
