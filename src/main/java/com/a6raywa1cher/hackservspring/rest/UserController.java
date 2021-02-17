4306e5c7618d0a96ebb0d143d663303872e683ca
GooS69 <guseff.daniil2011@yandex.ru>
2021-02-17 22:26:24 +0300

    hotfix

--- src/main/java/com/a6raywa1cher/hackservspring/rest/UserController.java
+++ src/main/java/com/a6raywa1cher/hackservspring/rest/UserController.java
@@ -11,6 +11,8 @@ import com.a6raywa1cher.hackservspring.service.dto.UserInfo;
 import com.a6raywa1cher.hackservspring.utils.LocalHtmlUtils;
 import com.a6raywa1cher.hackservspring.utils.Views;
 import com.fasterxml.jackson.annotation.JsonView;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
