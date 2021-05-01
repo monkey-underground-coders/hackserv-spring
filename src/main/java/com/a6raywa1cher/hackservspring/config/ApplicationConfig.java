4a3e67d64e3b6b28e9d35b59c0ec858e05728e62
Konstantin "6rayWa1cher" Grigorev <info@a6raywa1cher.com>
2021-05-01 18:37:51 +0300

    some patches

--- src/main/java/com/a6raywa1cher/hackservspring/config/ApplicationConfig.java
+++ src/main/java/com/a6raywa1cher/hackservspring/config/ApplicationConfig.java
@@ -6,11 +6,17 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.mail.internet.MimeMessage;
+import java.io.InputStream;
 
 @Configuration
 @EnableSpringDataWebSupport
@@ -26,6 +32,48 @@ public class ApplicationConfig {
 	@ConditionalOnProperty(prefix = "app", name = "email-verification", havingValue = "false")
 	@ConditionalOnMissingBean
 	public JavaMailSender javaMailSender() {
-		return new JavaMailSenderImpl();
+		return new JavaMailSender() {
+			private final JavaMailSender javaMailSender = new JavaMailSenderImpl();
+
+			@Override
+			public MimeMessage createMimeMessage() {
+				return javaMailSender.createMimeMessage();
+			}
+
+			@Override
+			public MimeMessage createMimeMessage(InputStream contentStream) throws MailException {
+				return javaMailSender.createMimeMessage(contentStream);
+			}
+
+			@Override
+			public void send(MimeMessage mimeMessage) throws MailException {
+
+			}
+
+			@Override
+			public void send(MimeMessage... mimeMessages) throws MailException {
+
+			}
+
+			@Override
+			public void send(MimeMessagePreparator mimeMessagePreparator) throws MailException {
+
+			}
+
+			@Override
+			public void send(MimeMessagePreparator... mimeMessagePreparators) throws MailException {
+
+			}
+
+			@Override
+			public void send(SimpleMailMessage simpleMessage) throws MailException {
+
+			}
+
+			@Override
+			public void send(SimpleMailMessage... simpleMessages) throws MailException {
+
+			}
+		};
 	}
 }
