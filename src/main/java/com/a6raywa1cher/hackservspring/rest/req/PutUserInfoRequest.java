24623e7b9b3c82be0a0e0cec19741c5b92b3c4cf
GooS69 <guseff.daniil2011@yandex.ru>
2021-05-05 15:40:23 +0300

    fix put user

--- src/main/java/com/a6raywa1cher/hackservspring/rest/req/PutUserInfoRequest.java
+++ src/main/java/com/a6raywa1cher/hackservspring/rest/req/PutUserInfoRequest.java
@@ -3,32 +3,29 @@ package com.a6raywa1cher.hackservspring.rest.req;
 import com.a6raywa1cher.hackservspring.utils.jackson.HtmlEscape;
 import lombok.Data;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
 public class PutUserInfoRequest {
-	@NotBlank
 	@HtmlEscape
 	private String firstName;
 
 	@HtmlEscape
 	private String middleName;
 
-	@NotBlank
 	@HtmlEscape
 	private String lastName;
 
-	@NotBlank
 	@Pattern(regexp = "^@(?=\\w{5,64}\\b)[a-zA-Z0-9]+(?:_[a-zA-Z0-9]+)*$")
 	@Size(max = 250)
 	private String telegram;
 
-	@NotNull
 	@Past
 	private LocalDate dateOfBirth;
 
-	@NotBlank
 	@HtmlEscape
 	private String workPlace;
 
